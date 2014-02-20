375d424cec8a7362ceb764d130b4addf501f4580
Indrek Ilves <minimalist6000@gmail.com>
2014-02-21 00:44:10 +0200

    Feature: Added ActiveIngredient

---
+++ src/main/java/com/crophealer/domain/ActiveIngredient.java
@@ -0,0 +1,33 @@
+package com.crophealer.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.ManyToOne;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class ActiveIngredient {
+
+    /**
+     */
+    private String latinName;
+
+    /**
+     */
+    private String comment;
+
+    /**
+     */
+    @ManyToOne
+    private Country country;
+
+    /**
+     */
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "activeIngredient")
+    private Set<ActiveIngredientTranslation> translations = new HashSet<ActiveIngredientTranslation>();
+}
---
+++ src/main/java/com/crophealer/domain/ActiveIngredientTranslation.java
@@ -0,0 +1,36 @@
+package com.crophealer.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.Size;
+import javax.persistence.ManyToOne;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class ActiveIngredientTranslation {
+
+    /**
+     */
+    private String name;
+
+    /**
+     */
+    @Size(max = 1000)
+    private String description;
+
+    /**
+     */
+    @Size(max = 1000)
+    private String warning;
+
+    /**
+     */
+    @ManyToOne
+    private ActiveIngredient activeIngredient;
+
+    /**
+     */
+    @ManyToOne
+    private Languages lang;
+}
