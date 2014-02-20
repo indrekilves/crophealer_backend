4b1701f2c4467434d6fe6d075cc2644448cc0a2d
Indrek Ilves <minimalist6000@gmail.com>
2014-02-21 00:27:06 +0200

    Feature: Added PlantPartPhaseSymptom and PlantPartPhaseProblem

---
+++ src/main/java/com/crophealer/domain/PlantPartPhaseProblem.java
@@ -0,0 +1,23 @@
+package com.crophealer.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.OneToMany;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class PlantPartPhaseProblem {
+
+    /**
+     */
+    private String comment;
+
+    /**
+     */
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "problem")
+    private Set<PlantPartPhaseSymptom> symptoms = new HashSet<PlantPartPhaseSymptom>();
+}
---
+++ src/main/java/com/crophealer/domain/PlantPartPhaseSymptom.java
@@ -0,0 +1,20 @@
+package com.crophealer.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.ManyToOne;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class PlantPartPhaseSymptom {
+
+    /**
+     */
+    private String comment;
+
+    /**
+     */
+    @ManyToOne
+    private PlantPartPhaseProblem problem;
+}
