02929e245b0a044431cc3a1d971f220fb83158f7
Roman Normann <roman@raintree.ee>
2014-04-01 22:29:43 +0300

    Testing if auto compile works

--- src/main/java/com/crophealer/data/ActiveIngredientLoader.java
+++ src/main/java/com/crophealer/data/ActiveIngredientLoader.java
@@ -8,57 +8,47 @@ import com.crophealer.domain.ActiveIngredient;
 import com.crophealer.domain.ActiveIngredientProduct;
 import com.crophealer.domain.Product;
 
-public class ActiveIngredientLoader extends GenericLoader 
-{
+public class ActiveIngredientLoader extends GenericLoader {
 	protected Integer activeSheetNum = 1;
-	
-	public ActiveIngredientLoader(SpreadSheetReader ssReader)
-	{
+
+	public ActiveIngredientLoader(SpreadSheetReader ssReader) {
 		super(ssReader);
 		this.setActiveSheetNum(this.activeSheetNum);
 	}
-	
-	
-	public void loadAIs()
-	{
+
+	// test code
+	public void loadAIs() {
 		List<String> ais = this.ssReader.getColumnAsArray(0, 4);
-		
-		for (int i = 0; i < ais.size(); i++) 
-		{
+
+		for (int i = 0; i < ais.size(); i++) {
 			ActiveIngredient ai;
-			if (ais.get(i).isEmpty()) return;
-			
-			TypedQuery<ActiveIngredient> aiq = ActiveIngredient.findActiveIngredientsByLatinNameEquals(ais.get(i));
-			if (aiq.getResultList().size() > 0)
-			{
+			if (ais.get(i).isEmpty())
+				return;
+
+			TypedQuery<ActiveIngredient> aiq = ActiveIngredient
+					.findActiveIngredientsByLatinNameEquals(ais.get(i));
+			if (aiq.getResultList().size() > 0) {
 				ai = aiq.getSingleResult();
-			}
-			else
-			{
+			} else {
 				ai = new ActiveIngredient();
 				ai.setLatinName(ais.get(i));
 				ai.persist();
-			}	
-			
+			}
+
 			List<String> aiDetails = this.ssReader.getRowAsArray(i + 4);
-			Product product = ProductLoader.loadProductWithDetails(aiDetails.get(1), aiDetails.get(2), aiDetails.get(3));
-			
+			Product product = ProductLoader.loadProductWithDetails(
+					aiDetails.get(1), aiDetails.get(2), aiDetails.get(3));
+
 			ActiveIngredientProduct aip = new ActiveIngredientProduct();
 			aip.setProduct(product);
 			aip.setActiveIngredient(ai);
 			aip.persist();
-			
-			
+
 		}
 	}
-	
-	
-	
-	
-	public void loadAITranslations()
-	{
-		
+
+	public void loadAITranslations() {
+
 	}
-	
-	
+
 }
